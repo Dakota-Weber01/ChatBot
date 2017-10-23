4af12a6487b5a280eed25b3fe9a2e688b929d020
Dakota Weber <dakota.weber01@gmail.com>
2017-10-23 13:49:16 -0600

    Added a few methods and if's to my model class.

--- src/chat/model/ChatBot.java
+++ src/chat/model/ChatBot.java
@@ -1,6 +1,30 @@
 package chat.model;
 
 public class ChatBot
+private void buildQuestions()		
+public String processConversation(String input)
 {
+	{	
+	{
+	return null;
+	}	
 
+}
+	public boolean lengthChecker(String input)
+	{
+		boolean validLength = false;
+		if (input != null && input.length() > 2)
+		{
+			validLength = true;
+		}
+		return validLength;
+	}
+	public boolean htmlTagChecker(String input)
+	{
+		return false;
+	}
+	public boolean userNameChecker(String input)
+	{
+		return false;
+	}
 }
