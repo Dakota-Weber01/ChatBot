b5144622ac3ff77857a9a281f234df456046bbe6
Dakota Weber <dakota.weber01@gmail.com>
2017-11-13 13:43:40 -0700

    added panel prereq's and other settings

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,5 +6,22 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatPanel appPane;
+	private ChatController appController;
+	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setSize(600, 600);
+		this.setVisible(true);
+		this.setResizable(false);
+	}
 }
