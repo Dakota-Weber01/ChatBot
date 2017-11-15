a816d39263d425e3883c46aa7285e5b29af124a6
Dakota Weber <dakota.weber01@gmail.com>
2017-11-15 12:26:10 -0700

    Setting up framework

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,25 @@
 package chat.view;
 
-public class ChatFrame
+import javax.swing.JFrame;
+import chat.controller.ChatController;
+
+public class ChatFrame extends JFrame
 {
+private ChatController appController;
+private ChatPanel appPanel;
+public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+//		this.ContentPane(appPanel);
+		this.setSize(500,500);
+		this.setTitle("Chatbot 2017");
+		this.setVisible(true);
+		this.setResizable(true);
+	}
 
 }
