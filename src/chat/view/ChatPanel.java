01b528a696dad76001a04f1b34bec6e826cef343
Dakota Weber <dakota.weber01@gmail.com>
2017-11-15 12:43:41 -0700

    Setting up basic GUI components for ChatPanel

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,36 @@
 package chat.view;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.JTextArea;
+import chat.controller.ChatController;
+import chat.view.ChatFrame;
+import java.awt.Color;
 
-public class ChatPanel
+public class ChatPanel extends JPanel
 {
-
+	private ChatController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	public ChatPanel(ChatController appController)
+	{
+		
+	}
+	private void setupPanel()
+	{
+		
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
+	}
+	
+	
 }
