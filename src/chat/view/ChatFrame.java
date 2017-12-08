9a8d18612c3dde9274f5867e4c664945b5309c2b
Dakota Weber <dakota.weber01@gmail.com>
2017-12-08 13:41:21 -0700

    fixed a class bug

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,32 +1,8 @@
 package chat.view;
 
-<<<<<<< HEAD
-=======
+
 import javax.swing.JFrame;
 import chat.controller.ChatController;
-
-public class ChatFrame extends JFrame
-{
-private ChatController appController;
-private ChatPanel appPanel;
-public ChatFrame(ChatController appController)
-	{
-		super();
-		this.appController = appController;
-		setupFrame();
-	}
-	private void setupFrame()
-	{
-//		this.ContentPane(appPanel);
-		this.setSize(500,500);
-		this.setTitle("Chatbot 2017");
-		this.setVisible(true);
-		this.setResizable(true);
-	}
->>>>>>> master
-
-import chat.controller.ChatController;
-import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
